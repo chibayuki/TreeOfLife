f524df80b0a52a80bb93533ee10906fe26d4486a
千夏茹初 <chibayuki@foxmail.com>
2021-04-24 18:34:45 +0800

    Add files via upload

---
+++ TreeOfLife/Views/About/ViewModel_About.cs
@@ -0,0 +1,62 @@
+﻿/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+Copyright © 2021 chibayuki@foxmail.com
+
+TreeOfLife
+Version 1.0.1100.1000.M11.210405-0000
+
+This file is part of TreeOfLife
+* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.ComponentModel;
+using System.IO;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace TreeOfLife.Views.About
+{
+    public class ViewModel_About
+    {
+        public ViewModel_About()
+        {
+        }
+
+        //
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void NotifyPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        //
+
+        public ImageSource AppLogo_256
+        {
+            get
+            {
+                BitmapImage bmp;
+
+                try
+                {
+                    bmp = new BitmapImage();
+                    bmp.BeginInit();
+                    bmp.StreamSource = new MemoryStream(Resource.AppLogo_256);
+                    bmp.EndInit();
+                }
+                catch
+                {
+                    bmp = null;
+                }
+
+                return bmp;
+            }
+        }
+    }
+}
---
+++ TreeOfLife/Views/About/View_About.xaml.cs
@@ -0,0 +1,41 @@
+﻿/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+Copyright © 2021 chibayuki@foxmail.com
+
+TreeOfLife
+Version 1.0.1100.1000.M11.210405-0000
+
+This file is part of TreeOfLife
+* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TreeOfLife.Views.About
+{
+    /// <summary>
+    /// View_About.xaml 的交互逻辑
+    /// </summary>
+    public partial class View_About : UserControl
+    {
+        public ViewModel_About ViewModel => this.DataContext as ViewModel_About;
+
+        //
+
+        public View_About()
+        {
+            InitializeComponent();
+        }
+    }
+}
